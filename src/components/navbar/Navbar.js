b7af647c6a11f30a69b0ff8fa0776881cf83bb6f
Fatiq1709 <fatiqasif7@gmail.com>
2023-06-15 11:05:10 +0500

    forth commit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './navbar.css';
-
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className='navContainer'>
+        <Link to={'/'}>
             <span className='logo'>lamabooking</span>
+            </Link>
             <div className='navItem'>
                 <button className='navButton'>Register</button>
                 <button className='navButton'>Login</button>
